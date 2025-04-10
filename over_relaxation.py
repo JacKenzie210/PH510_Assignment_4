919efcc51ba2e4ffff42ac88abafca4b1db22eaf
JacKenzie210 <jack.mackenzie.2020@uni.strath.ac.uk>
2025-04-10 18:52:29 +0100

    Update over_relaxation.py
    Initilised the grid with boundary value and initial guesses inside the grid.

--- over_relaxation.py
+++ over_relaxation.py
@@ -9,9 +9,34 @@ import matplotlib.pyplot as plt
 
 class OverRelaxation:
     
-    def __init__(self,size, spacing):
+    def __init__(self,grid_shape, spacing, x0):
         
-        self.N = size
+
         self.h = spacing
+        
+        self.grid = np.empty( grid_shape )
+        
+        self.grid[1:-1,:] = boundary_values
+        self.grid[:,1:-1] = boundary_values
+        
+        self.grid[1:-1, 1:-1] = x0
+        
+    def solve(self):
+        
+        phi = 1/4* (1) 
 
 
+###############################################################################
+# Initial Conditions 
+###############################################################################
+
+boundary_values = 1
+shape = (16,16)
+h = 1
+x0 = 0
+
+###############################################################################
+# Testing
+###############################################################################
+
+a = OverRelaxation(shape, h, x0)
