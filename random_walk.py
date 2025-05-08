81033cce30c864bacda2a1bc513d81b352a7b9e1
JacKenzie210 <jack.mackenzie.2020@uni.strath.ac.uk>
2025-05-08 18:04:06 +0100

    Update random_walk.py
    Made it work for any number of walks and set default perameters

--- random_walk.py
+++ random_walk.py
@@ -47,7 +47,7 @@ class RandomWalk:
 
         self.conv_tolerence = convergance_tolerence
                                                   
-    def random_walker(self,initial_position, direction_probs = None):
+    def random_walker(self,initial_position = None, n_walks = None, direction_probs = None):
         """
         The random walker used to determine the Greens function
         
@@ -55,16 +55,25 @@ class RandomWalk:
         ----------        
         initial_position : the co-ordinate of where to begin random  walks
         
-        direction_probs : list of Probabilities of going 
+        n_walks = number of walks. Used to determine number of times the while 
+                  loop is ran.
+        
+        direction_probs : 1D list (of length = 4) of Probabilities from going 
                          up/down/left/right respectively
         """
         #Greens grid is used to determine the probabilities 
         #(and thus greens function) of each boundary point bering hit
         greens_grid = np.zeros( self.grid_shape )
         
+        if initial_position is None:
+            initial_position = (len(greens_grid)//2 , len(greens_grid)//2)
+        
+        if n_walks is None:
+            #Defaulting number of walks to 100 if no imput is presented
+            n_walks = 100
 
         
-        if direction_probs == None:
+        if direction_probs is None:
             #assigns equal probability of each direction occuring
             direction_probs = np.array([0.25,
                                        0.25,
@@ -79,31 +88,41 @@ class RandomWalk:
         right = direction_probs[3] * 100
         
 
-        xi = initial_position[0]
-        yj = initial_position[1]
+
         
-        while not ( xi ==0 or xi ==len(greens_grid)-1 or yj == 0 or yj ==len(greens_grid)-1):
+        for i in range(n_walks):
             
-            direction = np.random.randint(0,101)
+            xi = initial_position[0]
+            yj = initial_position[1]      
             
-            if 0 <= direction <= up-1 :
-                xi -= 1 
-                print("U")
-            elif up <= direction <= up+down-1:
-                xi +=1
-                print("D")
+            while not ( xi ==0 or xi ==len(greens_grid)-1 or yj == 0 or yj ==len(greens_grid)-1):
                 
-            elif up + down <= direction <= up+down+left-1:
-                yj -= 1
-                print("L")
-            elif up+down+left <= direction <= up+down+left+right-1:
-                yj +=1
-                print("R")
-            
+                direction = np.random.randint(0,101)
+                
+                if 0 <= direction <= up-1 :
+                    xi -= 1 
+                    print(f'({xi},{yj}) U')
+                elif up <= direction <= up+down-1:
+                    xi +=1
+                    print(f'({xi},{yj}) D')
+                    
+                elif up + down <= direction <= up+down+left-1:
+                    yj -= 1
+                    print(f'({xi},{yj}) L')
+                elif up+down+left <= direction <= up+down+left+right-1:
+                    yj +=1
+                    print(f'({xi},{yj}) R')
+  
             greens_grid[xi,yj]+=1
             print(f'({xi},{yj})')
-            
+        
+        sum_of_hits = np.sum(greens_grid)
+
+        if sum_of_hits == n_walks:
+            print(f'All {n_walks} walks accounted for')
          
+        #Turning greens_grid from hits into probabilities
+        greens_grid = greens_grid/n_walks
         
         return greens_grid
         
@@ -117,7 +136,7 @@ class RandomWalk:
 ###############################################################################
 
 boundary_values = 5
-grid_size = 9
+grid_size = 10
 h = 1
 x0 = 3
 
@@ -126,10 +145,12 @@ epsilon = 1e-3
 test_func = 1
 
 ipos = (4,4)
+n_walks = 1000
 
+prob_walks = np.array([0.40,0.250,0.30,0.05])
 ###############################################################################
 # Testing
 ###############################################################################                               
 a = RandomWalk(grid_size, h, x0, boundary_values,epsilon,  test_func)
 
-b = a.random_walker(ipos)
+b = a.random_walker(ipos, n_walks, prob_walks )
