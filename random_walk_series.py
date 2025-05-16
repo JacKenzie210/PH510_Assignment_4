295efc61fcf3c2dfc488b7cb5f1d26ff47ce990f
JacKenzie210 <jack.mackenzie.2020@uni.strath.ac.uk>
2025-05-16 14:25:09 +0100

    pylint fixes

--- random_walk_series.py
+++ random_walk_series.py
@@ -80,6 +80,18 @@ class GreensFunc:
                                                   # by 0 error
 
     def std_greens(self, grid):
+        """
+
+
+        Parameters
+        ----------
+        grid : NxN grid
+
+        Returns
+        -------
+        std : standard deviation of the gridsnboundary values only
+
+        """
         up_bound = grid[0, 1:-1]
         low_bound = grid[-1, 1:-1]
         left_bound = grid[1:-1, 0]
@@ -102,7 +114,7 @@ class GreensFunc:
                   loop is ran.
 
         direction_probs : 1D list (of length = 4) of Probabilities from going
-                         up/down/left/right respectively
+                         UP/DOWN/LEFT/RIGHT respectively
         """
         #Greens grid is used to determine the probabilities
         #(and thus greens function) of each boundary point being hit from the
@@ -118,7 +130,6 @@ class GreensFunc:
             #Defaulting number of walks to 100 if no imput is presented
             n_walks = 100
 
-
         if direction_probs is None:
             #assigns equal probability of each direction occuring
             direction_probs = np.array([0.25,
@@ -129,16 +140,16 @@ class GreensFunc:
             raise ValueError("ensure sum of probabilities = 1 (decimal probs)")
 
         #converts from decimal to percentage
-        up = direction_probs[0] * 100
+        upward = direction_probs[0] * 100
         down = direction_probs[1] * 100
         left = direction_probs[2] * 100
-        right = direction_probs[3] * 100
+        #RIGHT = direction_probs[3] * 100
 
 
         if self.x_0 == 0 : # less computationally demanding to record only the
                      # boundary values of the walk when considering Laplace.
 
-            for _ in range(n_walks):
+            for _ in range(int(n_walks)):
 
                 x_i = int(initial_position[0])
                 y_j = int(initial_position[1])
@@ -148,14 +159,14 @@ class GreensFunc:
 
                     direction = np.random.randint(0,100)
 
-                    if 0 <= direction <= up-1 :
+                    if 0 <= direction <= upward-1 :
                         x_i -= 1
                         #print(f'({x_i},{y_j}) U')
-                    elif up <= direction <= up+down-1:
+                    elif upward <= direction <= upward+down-1:
                         x_i +=1
                         #print(f'({x_i},{y_j}) D')
 
-                    elif up + down <= direction <= up+down+left-1:
+                    elif upward + down <= direction <= upward+down+left-1:
                         y_j -= 1
                         #print(f'({x_i},{y_j}) L')
                     else:
@@ -179,7 +190,7 @@ class GreensFunc:
               # charges != 0
             n_steps = 0
             boundary_hits = 0
-            for i in range(n_walks):
+            for _ in range(int(n_walks)):
                 x_i = int(initial_position[0])
                 y_j = int(initial_position[1])
 
@@ -188,18 +199,18 @@ class GreensFunc:
 
                     direction = np.random.randint(0,100)
 
-                    if 0 <= direction <= up-1 :
+                    if 0 <= direction <= upward-1 :
                         x_i -= 1
                         self.greens_grid[x_i,y_j]+=1
                         n_steps +=1
                         #print(f'({x_i},{y_j}) U')
-                    elif up <= direction <= up+down-1:
+                    elif upward <= direction <= upward+down-1:
                         x_i +=1
                         self.greens_grid[x_i,y_j]+=1
                         n_steps +=1
                         #print(f'({x_i},{y_j}) D')
 
-                    elif up + down <= direction <= up+down+left-1:
+                    elif upward + down <= direction <= upward+down+left-1:
                         y_j -= 1
                         self.greens_grid[x_i,y_j]+=1
                         n_steps +=1
@@ -257,6 +268,24 @@ class GreensFunc:
 
     def solve(self, initial_position = None, n_walks = None,
               direction_probs = None):
+        """
+
+
+        Parameters
+        ----------
+        initial_position : tuple, coordinates for walks to originate from.
+        The default is None.
+        n_walks : int, number of walks to run. The default is None.
+        direction_probs : list of floats, specify direction weight if desired.
+        The default is None.
+
+        Returns
+        -------
+        the end result for the phi grid
+        the value of phi at the origin point i,j
+        std_phi : the standard deviation of phi
+
+        """
 
         if not hasattr(self, 'greens_grid'):#prevent recalculation
                                                  #if already conducted
@@ -289,8 +318,11 @@ class GreensFunc:
             std_green_charge = np.std(self.greens_grid[1:-1,1:-1])
 
             #grid for f_pq
-            rand_x = np.random.uniform(0, self.bound , size =self.n_grid_points)
-            rand_y = np.random.uniform(0, self.bound , size =self.n_grid_points)
+            rand_x = np.random.uniform(0, np.max(self.bound)
+                                       , size =self.n_grid_points)
+            rand_y = np.random.uniform(0, np.max(self.bound),
+                                       size =self.n_grid_points)
+
             mc_arr = np.array([rand_x,rand_y])
 
             function_grid = self.phi_grid * self.func(mc_arr)
@@ -306,23 +338,6 @@ class GreensFunc:
 
 
         return self.phi_grid, self.phi_ij, std_phi
-
-
-
-
-
-    def grid_coords(self, grid):
-
-        """
-        generation of a meshgrid to create arrays representing the co-ords
-        of the inputted grid. Allows for easy index_ing for f(x,y) in the class.
-        """
-
-        #This generates the co-ordinates for the grid positions
-        row, column = np.indices((self.n_grid_points, self.n_grid_points))
-        coords = np.vstack([row.ravel(), column.ravel()])
-
-        return coords*self.h_space
 
 
     # def plot3d(self,initial_position = None,
@@ -355,6 +370,10 @@ class GreensFunc:
 ###############################################################################
 if __name__ == "__main__":
     def test_func(coords):
+        """
+        takes a co-ordinates array of shape(dim, N_cords)
+        returns the test function (changed as required)
+        """
         return np.sin(np.sum(coords))**2
 
 
@@ -395,40 +414,40 @@ if __name__ == "__main__":
 # Task 3
 ###############################################################################
 if __name__ == "__main__":
-    grid_size_t3 = 10e-2 #m
-    h_t3 = 1e-3 # m  or 1e-1 cm
-    x0_t3 = 1 #using poissons Green functions.
-    boundary_t3 = 3
-    n_walks_t3 = 10000
+    GRID_SIZE_T3 = 10e-2 #m
+    H_T3 = 1e-3 # m  or 1e-1 cm
+    X0_T3 = 1 #using poissons Green functions.
+    BOUNDARY_T3 = 3
+    N_WALKS_T3 = 10000
     point_a = (5e-2,5e-2) #cm
     point_b = (2.5e-2,2.5e-2) #cm
     point_c = (0.1e-2, 2.5e-2) #cm
     point_d = (0.1e-2, 0.1e-2) #cm
     points_t3 = np.array([point_a,point_b,point_c,point_d])
 
-    # task_3_sol = np.zeros(len(points_t3))
-    # t3_std = np.zeros(len(points_t3))
+    task_3_sol = np.zeros(len(points_t3))
+    t3_std = np.zeros(len(points_t3))
 
 
-    # for i in range(len(points_t3)):
-    #     initialise = GreensFunc(grid_size_t3,h_t3,x0_t3,boundary_t3, test_func)
-    #     t3_walker = initialise.random_walker(points_t3[i],n_walks_t3)
-    #     t3_grid = t3_walker[0]
-    #     t3_std[i]= t3_walker[1]
+    for i in range(len(points_t3)):
+        initialise = GreensFunc(GRID_SIZE_T3,H_T3,X0_T3,BOUNDARY_T3, test_func)
+        t3_walker = initialise.random_walker(points_t3[i],N_WALKS_T3)
+        t3_grid = t3_walker[0]
+        t3_std[i]= t3_walker[1]
 
 
-    #     fig, ax = plt.subplots()
+        fig, ax = plt.subplots()
 
-    #     #full grid and boundaries
-    #     imshow = ax.imshow(t3_grid, cmap="inferno", vmax = 0.004)
-    #     ax.set_title(f"greens function at point {points_t3[i]/grid_size_t3} cm")
-    #     ax.set_xlabel(f'y grid_point ({grid_size_t3}cm/{h_t3} spacing)')
-    #     ax.set_ylabel(f'x grid_point ({grid_size_t3}cm/{h_t3} spacing)')
-    #     fig.colorbar(imshow)
+        #full grid and boundaries
+        imshow = ax.imshow(t3_grid, cmap="inferno", vmax = 0.004)
+        ax.set_title(f"greens function at point {points_t3[i]} cm")
+        ax.set_xlabel(f'y grid_point ({GRID_SIZE_T3}cm/{H_T3} spacing)')
+        ax.set_ylabel(f'x grid_point ({GRID_SIZE_T3}cm/{H_T3} spacing)')
+        fig.colorbar(imshow)
 
-    # print('\nTask 3\n-------\nsee graphs')
-    # for j in range(len(points_t3)):
-    #     print(f'std = {t3_std[j]}')
+    print('\nTask 3\n-------\nsee graphs')
+    for j in range(len(points_t3)):
+        print(f'std = {t3_std[j]}')
 
 
 
@@ -440,90 +459,124 @@ if __name__ == "__main__":
 
     print('\nTask 4\n-------\n')
     #taking the perameters from task 3
-    grid_size_t4 = 10e-2 #m
-    h_t4 = 1e-3 # m  or 1e-1 cm
-    x0_t4 = 0
-    n_walks_t4 = 1e8
+    GRID_SIZE_T4 = 10e-2 #m
+    H_T4 = 1e-3 # m  or 1e-1 cm
+    X0_T4 = 0
+    N_WALKS_T4 = 1e2
 
     prob_walks = np.array([0.25,0.25,0.25,0.25])
     points_t4 = points_t3.copy()
     def charge_func(coords):
-        return 1
+        """
+
+        Parameters
+        ----------
+        coords : the coordinate in shape (dim, N_cords)
+
+        Returns
+        -------
+        returns the  charge function for each value for use in the Monte Carlo
+        integral.
+
+        """
+        return coords[0]+coords[1]
     print('Laplace grid')
 
 
-    def task_4_bounds(x0_task4, i):
+    def task_4_bounds(x0_task4, point_i):
+
+        """
+        i : number of itteration to select the correct origin point
+
+        Returns
+        -------
+        task_4a soluion
+        task_4b solution
+        task_4c solution
+
+        """
         # part a
         boundary_t4 = 1
 
-        task_4a = GreensFunc(grid_size_t4, h_t4, x0_t4, boundary_t4, charge_func)
-        #task_4a_greens = task_4a.random_walker(points_t4[i], n_walks_t4)
-        task_4a_solve = task_4a.solve(points_t4[i], n_walks_t4)
+        task_4a = GreensFunc(GRID_SIZE_T4, H_T4, x0_task4, boundary_t4, charge_func)
+        #task_4a_greens = task_4a.random_walker(points_t4[i], N_WALKS_T4)
+        task_4a_solve = task_4a.solve(points_t4[point_i], N_WALKS_T4)
         print(f'a) Phi_ij = {task_4a_solve[1]} +/- {task_4a_solve[2]}')
 
         #part b
         boundary_t4 = np.array([1,1,-1,-1])
 
-        task_4b = GreensFunc(grid_size_t4, h_t4, x0_t4, boundary_t4, charge_func)
-        #task_4b_greens = task_4b.random_walker(points_t4[i], n_walks_t4)
-        task_4b_solve = task_4b.solve(points_t4[i], n_walks_t4)
+        task_4b = GreensFunc(GRID_SIZE_T4, H_T4, x0_task4, boundary_t4, charge_func)
+        #task_4b_greens = task_4b.random_walker(points_t4[i], N_WALKS_T4)
+        task_4b_solve = task_4b.solve(points_t4[point_i], N_WALKS_T4)
         print(f'b) Phi_ij = {task_4b_solve[1]} +/- {task_4b_solve[2]}')
 
         #part c
         boundary_t4 = np.array([2,2,1,-4])
 
-        task_4c = GreensFunc(grid_size_t4, h_t4, x0_t4, boundary_t4, charge_func)
-        #task_4c_greens = task_4c.random_walker(points_t4[i], n_walks_t4)
-        task_4c_solve = task_4c.solve(points_t4[i], n_walks_t4)
+        task_4c = GreensFunc(GRID_SIZE_T4, H_T4, x0_task4, boundary_t4, charge_func)
+        #task_4c_greens = task_4c.random_walker(points_t4[i], N_WALKS_T4)
+        task_4c_solve = task_4c.solve(points_t4[point_i], N_WALKS_T4)
         print(f'c) Phi_ij = {task_4c_solve[1]} +/- {task_4c_solve[2]}')
         #part d
         boundary_t4 = np.array([2,2,1,-4])
 
-        task_4c = GreensFunc(grid_size_t4, h_t4, x0_t4, boundary_t4, charge_func)
-        #task_4c_greens = task_4c.random_walker(points_t4[0], n_walks_t4)
-        task_4c_solve = task_4c.solve(points_t4[0], n_walks_t4)
+        task_4c = GreensFunc(GRID_SIZE_T4, H_T4, X0_T4, boundary_t4, charge_func)
+        #task_4c_greens = task_4c.random_walker(points_t4[0], N_WALKS_T4)
+        task_4c_solve = task_4c.solve(points_t4[point_i], N_WALKS_T4)
         print(f'b) Phi_ij = {task_4c_solve[1]} +/- {task_4c_solve[2]} \n\n')
 
         return task_4a_solve,task_4b_solve,task_4c_solve
 
-    print('Poission grid')
-    def task_4_charge(i):
+
+    def task_4_charge(point_i):
+        """
+        i : number of itteration to select the correct origin point
+
+        Returns
+        -------
+        task_4d soluion
+        task_4e solution
+        task_4f solution
+
+        """
 
         #part d
         x0_d = 10 # C
-        task_4d = task_4_bounds(x0_d,i)
+        task_4d = task_4_bounds(x0_d,point_i)
 
-        x0_e = np.linspace(1,0, int(grid_size_t4//h_t3)-2) # -2 for interior size
-        x0_e = np.outer(np.linspace(1, 0, int(grid_size_t4//h_t3)-2),
-                        np.ones(int(grid_size_t4//h_t3)-2))
+        x0_e = np.linspace(1,0, int(GRID_SIZE_T4//H_T3)-2) # -2 for interior size
+        x0_e = np.outer(np.linspace(1, 0, int(GRID_SIZE_T4//H_T3)-2),
+                        np.ones(int(GRID_SIZE_T4//H_T3)-2))
 
-        task_4e = task_4_bounds(x0_e,i)
+        task_4e = task_4_bounds(x0_e,point_i)
 
 
-        x_f = np.linspace(0, grid_size_t4, int(grid_size_t4//h_t3)-2)
-        y_f = np.linspace(0, grid_size_t4, int(grid_size_t4//h_t3)-2)
+        x_f = np.linspace(0, GRID_SIZE_T4, int(GRID_SIZE_T4//H_T3)-2)
+        y_f = np.linspace(0, GRID_SIZE_T4, int(GRID_SIZE_T4//H_T3)-2)
 
-        X_f,Y_f = np.meshgrid(x_f,y_f)
+        x_grid,y_grid = np.meshgrid(x_f,y_f)
 
-        r_f  = np.sqrt(X_f**2+Y_f**2)
+        r_f  = np.sqrt(x_grid**2+y_grid**2)
         x0_f = np.exp(-2000*r_f)
 
-        task_4f = task_4_bounds(x0_f,i)
+        task_4f = task_4_bounds(x0_f,point_i)
 
 
         return task_4d,task_4e,task_4f
 
     #solving Task 4 for each of the T3 points
-    solutions_abc_55 = task_4_bounds(x0_t4,0)
+    print('point (5,5) cm')
+    solutions_abc_55 = task_4_bounds(X0_T4,0)
     solutions_def_55 = task_4_charge(0)
 
-    solutions_abc_25 = task_4_bounds(x0_t4,1)
+    print('point (2.5,2.5) cm')
+    solutions_abc_25 = task_4_bounds(X0_T4,1)
     solutions_def_25 = task_4_charge(1)
 
-    solutions_abc_0125 = task_4_bounds(x0_t4,2)
+    print('point (0.1,0.25) cm')
+    solutions_abc_0125 = task_4_bounds(X0_T4,2)
     solutions_def_0125 = task_4_charge(2)
-
-    solutions_abc_0101 = task_4_bounds(x0_t4,3)
+    print('point (0.1,0.1) cm')
+    solutions_abc_0101 = task_4_bounds(X0_T4,3)
     solutions_def_0101 = task_4_charge(3)
-
-
